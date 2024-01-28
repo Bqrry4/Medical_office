a50e7906ee30c683570b52c09c18b610dc4dd2ff
Bqrry <bqrry4@gmail.com>
2024-01-28 15:30:51 +0200

    React: done patient and physician view and update

--- React/medical_office/src/app/page.tsx
+++ React/medical_office/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import axios from "axios";
 
 const backHost = "localhost:8080" 
@@ -7,14 +7,18 @@ const backHost = "localhost:8080"
 export default function App() {
 
   const [isLoggedIn, setLoggedIn] = useState(false);
-  const [role, setRole] = useState("patient");
+  // const [role, setRole] = useState("patient");
 
   let content;
 
+  useEffect(()=>{
+    setLoggedIn(localStorage.getItem("authToken") != null)
+    }, [isLoggedIn])
+
+
    content = (!isLoggedIn) ? 
     <Login setLoggedIn = {setLoggedIn} /> :
-    role == "patient" && <Patient /> ||
-    role == "physician" && <Physician />
+    <Home setLoggedIn = {setLoggedIn}/>
 
   return (
     <div>
@@ -36,7 +40,7 @@ function Login({ setLoggedIn }) {
         password
       })
       .then((response) => {
-        localStorage.setItem("token", response.data)
+        localStorage.setItem("authToken", response.data)
         setLoggedIn(true)
       })
       .catch((error) => {
@@ -69,20 +73,98 @@ function Login({ setLoggedIn }) {
   );
 }
 
-
-function Patient()
+function Home({ setLoggedIn })
 {
+  const [userData, setUserData] = useState({});
+  const [links, setLinks] = useState({});
 
+  useEffect(()=>{
+    axios
+    .get(`http://${backHost}/api/medical_office/self`, {
+      headers: {
+          Authorization: "Bearer " + localStorage.getItem("authToken"),
+      }})
+    .then((response) => {
+      setLinks(response.data._links)
+      delete response.data._links
+      setUserData(response.data)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+    
+    }, [])
+  
+
+    function readOnlyKeyFilter(key: string): boolean
+    {
+      return (key == "userID" || key == "cnp" || key == "physicianId" || key == "isActive");
+    }
+
+    function updateUserData()
+    {
+
+      const formData = new FormData(document.getElementById("userDataForm") as HTMLFormElement);
+      const newData = (Object.fromEntries(formData));
+
+      delete newData.cnp
+      delete newData.physicianID
+      
+
+      console.log(newData)
+
+      axios
+      .put(`http://${backHost}/api/medical_office/self`, newData, {
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("authToken"),
+        }})
+      .then((response) => {
+        alert("Succesful")
+      })
+      .catch((error) => {
+         alert(error.response.status + " : " + error.response.data)
+      });
+    }
+    
   return (
-    <div>pas</div>
+    <div> 
+         <LogoutButton setLoggedIn = {setLoggedIn}/>
+
+      <form className="centered" id="userDataForm">
+        { Object.entries(userData).map(([key,value],i) => 
+        <React.Fragment key={i}>
+          <label>{key}</label>
+          <input type={(key == "birthDay") ? "date" : "text"} name={key} key={i} defaultValue={value as string} readOnly={ readOnlyKeyFilter(key) }></input>
+        </React.Fragment>
+
+        )}       
+        <button type="button" onClick={updateUserData}>Update</button>
+      </form>
+    </div>
   )
 }
 
 
-function Physician()
+function LogoutButton({ setLoggedIn })
 {
+  
+  function performLogout()
+  {
+    axios
+    .put(`http://${backHost}/api/logout`, null, {
+      headers: {
+          Authorization: "Bearer " + localStorage.getItem("authToken"),
+      }})
+    .then((response) => {
+      localStorage.removeItem("authToken")
+      setLoggedIn(false)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
 
-  return (
-    <div>pasfas</div>
+  return(
+    <button id="logout" onClick={performLogout}>Logout</button>
   )
 }
